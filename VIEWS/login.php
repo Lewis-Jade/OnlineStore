da04a8c9b157c02c562cd6e748d2929623397e1f
Lewis <mudaidalewis@gmail.com>
2025-03-13 16:13:35 +0300

    Login form complete

---
+++ VIEWS/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <div class="container">
+        <section  class="cart">
+            <h1> welcome to Devv Electronics</h1>
+        </section>
+        <section  class="form-section">
+         <form action="">
+         <h2>Hello Again!</h2>
+         <h5>Welcme Back</h5>
+         <div class="inputBox">
+         <i class="fa-solid fa-envelope"></i>
+         
+            <input type="text" placeholder="Email">
+         </div>
+         
+         <div class="inputBox">
+         <i class="fa-solid fa-lock"></i>
+         <input type="password"  placeholder="Password">
+  
+         </div>
+         <input type="submit"  value="Login">
+         <div class="links">
+            <a href="#">Forgot password?</a>
+            <a href="#">Don't have an Account yet?</a>
+         </div>
+         
+         </form>
+        
+
+        </section>
+    </div>
+</body>
+</html>
